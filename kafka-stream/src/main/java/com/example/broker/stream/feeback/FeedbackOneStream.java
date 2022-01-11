5785b1db2ae9d99deafd166a74096fab0df93f2a
감동기 <dk.kam@tg360tech.com>
2022-01-11 12:48:25 +0900

    12. Kafak Stream - Feedback / 3. Good Feedback Or Bad Feedback

--- kafka-stream/src/main/java/com/example/broker/stream/feeback/FeedbackOneStream.java
+++ kafka-stream/src/main/java/com/example/broker/stream/feeback/FeedbackOneStream.java
@@ -4,10 +4,7 @@ import com.example.broker.message.FeedbackMessage;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Printed;
-import org.apache.kafka.streams.kstream.ValueMapper;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.support.serializer.JsonSerde;
@@ -20,40 +17,38 @@ import java.util.stream.Collectors;
 public class FeedbackOneStream {
 
     private static final Set<String> GOOD_WORDS = Set.of("happy", "good", "helpful");
+    private static final Set<String> BAD_WORDS = Set.of("angry", "sad", "bad");
 
     @Bean
-    public KStream<String, String> kstreamFeedback(StreamsBuilder builder) {
+    public KStream<String, FeedbackMessage> kstreamFeedback(StreamsBuilder builder) {
         var stringSerde = Serdes.String();
         var feedbackSerde = new JsonSerde<>(FeedbackMessage.class);
 
-//        var goodFeedbackSteam = builder.stream("t.commodity.feedback", Consumed.with(stringSerde, feedbackSerde))
-//                .flatMapValues(mapperGoodWords());
-        var feedbackSteam = builder.stream("t.commodity.feedback", Consumed.with(stringSerde, feedbackSerde));
-        feedbackSteam.print(Printed.<String, FeedbackMessage>toSysOut().withLabel("Feedback"));
+        var sourceStream = builder.stream("t.commodity.feedback", Consumed.with(stringSerde, feedbackSerde));
+        var feedbackStreams = sourceStream.flatMap(splitWords()).branch(isGoodWord(), isBadWord());
 
-        var goodFeedbackSteam = feedbackSteam.flatMap((k, v) -> {
-            return Arrays.asList(v.getFeedback().replaceAll("[^a-zA-Z]", " ").toLowerCase().split("\\s+")).stream()
-                    .filter(word -> GOOD_WORDS.contains(word))
-                    .distinct()
-                    .map(goodWord -> KeyValue.pair(v.getLocation(), goodWord))
-                    .collect(Collectors.toList());
-                }
-            );
+        feedbackStreams[0].print(Printed.<String, String>toSysOut().withLabel("Good"));
+        feedbackStreams[1].print(Printed.<String, String>toSysOut().withLabel("Bad"));
 
-        goodFeedbackSteam.print(Printed.<String, String>toSysOut().withLabel("Good Feedback"));
+        feedbackStreams[0].to("t.commodity.feedback-good");
+        feedbackStreams[1].to("t.commodity.feedback-bad");
 
-        goodFeedbackSteam.to("t.commodity.feedback-good");
-
-        return goodFeedbackSteam;
+        return sourceStream;
     }
 
-    private ValueMapper<FeedbackMessage, Iterable<String>> mapperGoodWords() {
-        return feedbackMessage -> Arrays.asList(
-                feedbackMessage.getFeedback()
-                        .replace("[^a-zA-A]", " ").toLowerCase()
-                        .split("\\s+")).stream()
-                .filter(word -> GOOD_WORDS.contains(word))
+    private Predicate<? super String,? super String> isBadWord() {
+        return (key, value) -> BAD_WORDS.contains(value);
+    }
+
+    private Predicate<? super String,? super String> isGoodWord() {
+        return (key, value) -> GOOD_WORDS.contains(value);
+    }
+
+    private KeyValueMapper<String, FeedbackMessage, Iterable<KeyValue<String, String>>> splitWords() {
+        return (k, v) -> Arrays.asList(v.getFeedback().replaceAll("[^a-zA-Z]", " ").split("\\s+")).stream()
+                .map(String::toLowerCase)
                 .distinct()
+                .map(word -> KeyValue.pair(v.getLocation(), word))
                 .collect(Collectors.toList());
     }
 }
